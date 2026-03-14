99a8cfd5dfff2e4df3c004528059ee188986ccaf
Calin Rasiga <rasigacalin@gmail.com>
2026-03-14 21:28:15 +0100

    chore: Finished the connector, config and container for testing connector.py and db_ops.py

--- scrape_supermarket_websites/matval_pipeline/tests/conftest.py
+++ scrape_supermarket_websites/matval_pipeline/tests/conftest.py
@@ -1,16 +1,30 @@
+from collections.abc import Generator
+
 import pytest
 from testcontainers.postgres import PostgresContainer
 
-import psycopg
-from psycopg import Connection
+from matval_pipeline.config import PostgresConfig
+from matval_pipeline.connector import PostgresConnector
+
 
 @pytest.fixture(scope="session")
-def pg_container():
-    with PostgresContainer(image="postgres:16", username="test", password="test", dbname="test") as pg: #noqa: S106, the password is a default for testing
+def pg_container() -> Generator[PostgresContainer, None, None]:
+    with PostgresContainer(image="postgres:16", username="test", password="test", dbname="test") as pg:  # noqa: S106
         yield pg
 
-@pytest.fixture(scope="session")
-def pg_connection() -> Connection[any]:
-    pg_connector = psycopg.connect()
 
-    return pg_connector
+@pytest.fixture(scope="session")
+def pg_config(pg_container: PostgresContainer) -> PostgresConfig:
+    return PostgresConfig(
+        host=pg_container.get_container_host_ip(),
+        port=int(pg_container.get_exposed_port()),
+        dbname=pg_container.dbname,
+        user=pg_container.username,
+        password=pg_container.password,
+    )
+
+
+@pytest.fixture(scope="session")
+def pg_connector(pg_config: PostgresConfig) -> Generator[PostgresConnector, None, None]:
+    with PostgresConnector(pg_config) as connector:
+        yield connector
